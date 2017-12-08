cee637fba735a97f0eadb4afae67c13ef48a4377
chiew <zmhbest@sina.cn>
2017-12-08 13:35:27 +0800

    add

---
+++ src/MNIST.java
@@ -0,0 +1,10 @@
+public class MNIST {
+    public static void main(String args[]){
+        WindowOperation win = new WindowOperation();
+        win.setSize(800,500);
+        win.setVisible(true);
+        win.setLocationRelativeTo(null);
+        win.setResizable(false);
+
+    }
+}
